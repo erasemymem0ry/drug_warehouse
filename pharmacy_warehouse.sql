1c559123e971adfccafaa37aac75043cc1972162
erasemymem0ry <159524096+erasemymem0ry@users.noreply.github.com>
2025-03-17 19:09:25 +0300

    Update pharmacy_warehouse.sql

--- pharmacy_warehouse.sql
+++ pharmacy_warehouse.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Время создания: Фев 26 2025 г., 10:50
+-- Время создания: Мар 17 2025 г., 17:08
 -- Версия сервера: 10.4.32-MariaDB
 -- Версия PHP: 8.2.12
 
@@ -59,7 +59,7 @@ INSERT INTO `distribution` (`distribution_id`, `branch_id`, `drug_id`, `employee
 CREATE TABLE `drugs` (
   `drug_id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `manufacturer` varchar(255) NOT NULL,
+  `manufacturer_id` int(11) NOT NULL,
   `purpose` varchar(255) NOT NULL,
   `category` varchar(255) NOT NULL,
   `unit_of_measurement` varchar(50) NOT NULL DEFAULT 'шт',
@@ -71,12 +71,33 @@ CREATE TABLE `drugs` (
 -- Дамп данных таблицы `drugs`
 --
 
-INSERT INTO `drugs` (`drug_id`, `name`, `manufacturer`, `purpose`, `category`, `unit_of_measurement`, `purchase_price`, `selling_price`) VALUES
-(1, 'Амоксициллин', 'ФармаКорп', 'Антибактериальный', 'Антибиотики', 'таблетки', 100.00, 150.00),
-(2, 'Парацетамол', 'Здоровье', 'Жаропонижающее', 'Обезболивающие', 'таблетки', 20.00, 35.00),
-(3, 'Ибупрофен', 'МедФарм', 'Противовоспалительное', 'Обезболивающие', 'капсулы', 30.00, 50.00),
-(4, 'Цефтриаксон', 'ФармаИнтер', 'Антибактериальный', 'Антибиотики', 'шт', 150.00, 200.00),
-(5, 'Аспирин', 'Здоровье', 'Жаропонижающее', 'Обезболивающие', 'таблетки', 10.00, 22.00);
+INSERT INTO `drugs` (`drug_id`, `name`, `manufacturer_id`, `purpose`, `category`, `unit_of_measurement`, `purchase_price`, `selling_price`) VALUES
+(1, 'Амоксициллин', 4, 'Антибактериальный', 'Антибиотики', 'таблетки', 100.00, 150.00),
+(2, 'Парацетамол', 1, 'Жаропонижающее', 'Обезболивающие', 'таблетки', 20.00, 35.00),
+(3, 'Ибупрофен', 3, 'Противовоспалительное', 'Обезболивающие', 'капсулы', 30.00, 50.00),
+(4, 'Цефтриаксон', 2, 'Антибактериальный', 'Антибиотики', 'шт', 150.00, 200.00),
+(5, 'Аспирин', 1, 'Жаропонижающее', 'Обезболивающие', 'таблетки', 10.00, 22.00);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `manufacturer`
+--
+
+CREATE TABLE `manufacturer` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Дамп данных таблицы `manufacturer`
+--
+
+INSERT INTO `manufacturer` (`id`, `name`) VALUES
+(1, 'Здоровье'),
+(2, 'МедФарм'),
+(3, 'ФармаКорп'),
+(4, 'ФармаИнтер');
 
 -- --------------------------------------------------------
 
@@ -194,7 +215,14 @@ ALTER TABLE `distribution`
 -- Индексы таблицы `drugs`
 --
 ALTER TABLE `drugs`
-  ADD PRIMARY KEY (`drug_id`);
+  ADD PRIMARY KEY (`drug_id`),
+  ADD KEY `FK1` (`manufacturer_id`);
+
+--
+-- Индексы таблицы `manufacturer`
+--
+ALTER TABLE `manufacturer`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Индексы таблицы `pharmacy_branches`
@@ -237,7 +265,13 @@ ALTER TABLE `distribution`
 -- AUTO_INCREMENT для таблицы `drugs`
 --
 ALTER TABLE `drugs`
-  MODIFY `drug_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `drug_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT для таблицы `manufacturer`
+--
+ALTER TABLE `manufacturer`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT для таблицы `pharmacy_branches`
@@ -274,6 +308,12 @@ ALTER TABLE `distribution`
   ADD CONSTRAINT `distribution_ibfk_1` FOREIGN KEY (`branch_id`) REFERENCES `pharmacy_branches` (`branch_id`),
   ADD CONSTRAINT `distribution_ibfk_2` FOREIGN KEY (`drug_id`) REFERENCES `drugs` (`drug_id`),
   ADD CONSTRAINT `distribution_ibfk_3` FOREIGN KEY (`employee_id`) REFERENCES `warehouse_employees` (`employee_id`);
+
+--
+-- Ограничения внешнего ключа таблицы `drugs`
+--
+ALTER TABLE `drugs`
+  ADD CONSTRAINT `FK1` FOREIGN KEY (`manufacturer_id`) REFERENCES `manufacturer` (`id`) ON UPDATE CASCADE;
 
 --
 -- Ограничения внешнего ключа таблицы `supplies`
